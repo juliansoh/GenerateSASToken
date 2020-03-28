5a483d2f8cddb69f81755f3c12c903d447432e3b
Julian <31254706+juliansoh@users.noreply.github.com>
2020-03-28 12:36:03 -0700

    Added project (Working version)
    Working version

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿//
+//This console application generates a SAS token for use with
+//Azure Event Hubs. Use the generated token in Postman or other code
+//Julian Soh
+//Microsoft Corp.
+//3/27/2020
+//
+
+using System;
+using System.Web;
+using System.Security.Cryptography;
+using System.Text;
+using System.Globalization;
+
+namespace GenerateSASToken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            string sasToken = createToken(args[0], args[1], args[2]);
+            Console.WriteLine(sasToken);
+            return;
+        }
+        private static string createToken(string resourceUri, string keyName, string key)
+        {
+            TimeSpan sinceEpoch = DateTime.UtcNow - new DateTime(1970, 1, 1);
+            var week = 60 * 60 * 24 * 7;
+            var expiry = Convert.ToString((int)sinceEpoch.TotalSeconds + week);
+            string stringToSign = HttpUtility.UrlEncode(resourceUri) + "\n" + expiry;
+            HMACSHA256 hmac = new HMACSHA256(Encoding.UTF8.GetBytes(key));
+            var signature = Convert.ToBase64String(hmac.ComputeHash(Encoding.UTF8.GetBytes(stringToSign)));
+            var sasToken = String.Format(CultureInfo.InvariantCulture, "SharedAccessSignature sr={0}&sig={1}&se={2}&skn={3}", HttpUtility.UrlEncode(resourceUri), HttpUtility.UrlEncode(signature), expiry, keyName);
+            return sasToken;
+        }
+    }
+}
